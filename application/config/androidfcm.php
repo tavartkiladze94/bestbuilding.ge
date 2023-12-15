52beb657699f7432087c028cdacecbd034710afb
Mister94 <153375790+tavartkiladze94@users.noreply.github.com>
2023-12-15 23:09:38 +0400

    Update androidfcm.php

--- application/config/androidfcm.php
+++ application/config/androidfcm.php
@@ -14,7 +14,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | The secret key for Firebase API
 |
  */
-$config['key'] = 'AAAA-bDjErY:APA91bE8FUQixvp0J-YqXR-_FruRNLiv-IBbqyiBSVoNy-KZmceguPfYWZ9auleUqXA6UxjH_CDgsyT0GNsa2Yi6USH_OpP8Jf_H4ekiMWmnP3GI0iwUCJAzuEuAaIUKA6z2qw6XU2EF';
+$config['key'] = '';
 
 /*
 |--------------------------------------------------------------------------
